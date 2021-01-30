f704fe27393f3c1190c8c0e226e33b96c7812572
Hsuan-Yu Chou <syuanyujhou@gmail.com>
2021-01-31 00:30:39 +0800

    looks good for int.

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,15 +1,13 @@
-export const appointments = [
+export const data = [
   {
     title: "Check-in",
     startDate: new Date(2021, 4, 8, 9),
-    endDate: new Date(2021, 4, 8, 10),
+    // endDate: new Date(2021, 4, 8, 10),
     // id: 0,
-    // location: "Room 1",
   },
   {
     title: "Opening",
     startDate: new Date(2021, 4, 8, 10),
-    endDate: new Date(2021, 4, 8, 11),
   },
   {
     title: "Making",
@@ -24,46 +22,37 @@ export const appointments = [
   {
     title: "Lunch",
     startDate: new Date(2021, 4, 8, 12),
-    endDate: new Date(2021, 4, 8, 13),
   },
   {
     title: "Dinner",
     startDate: new Date(2021, 4, 8, 18),
-    endDate: new Date(2021, 4, 8, 19),
   },
   {
     title: "Night Treat",
     startDate: new Date(2021, 4, 8, 21),
-    endDate: new Date(2021, 4, 8, 22),
   },
   {
     title: "Breakfast",
     startDate: new Date(2021, 4, 9, 7),
-    endDate: new Date(2021, 4, 9, 8),
   },
   {
     title: "Lunch",
     startDate: new Date(2021, 4, 9, 11, 30),
-    endDate: new Date(2021, 4, 9, 12),
   },
   {
     title: "Expo",
     startDate: new Date(2021, 4, 9, 12),
-    endDate: new Date(2021, 4, 9, 13),
   },
   {
     title: "Top 8 Demo",
     startDate: new Date(2021, 4, 9, 16),
-    endDate: new Date(2021, 4, 9, 17),
   },
   {
     title: "Award Ceremony",
     startDate: new Date(2021, 4, 9, 17),
-    endDate: new Date(2021, 4, 9, 18),
   },
   {
     title: "Ending",
     startDate: new Date(2021, 4, 9, 18),
-    endDate: new Date(2021, 4, 9, 19),
   },
 ];
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,97 +6,30 @@ import {
   DayView,
   Appointments,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { green } from "@material-ui/core/colors";
-// import { appointments } from "./data";
 
-const theme = createMuiTheme({ palette: { type: "light", primary: green } });
+import { data } from "./data";
 
-const appointments = [
-  {
-    title: "Check-in",
-    startDate: new Date(2021, 4, 8, 9),
-    endDate: new Date(2021, 4, 8, 10),
-    // id: 0,
-    // location: "Room 1",
-  },
-  {
-    title: "Opening",
-    startDate: new Date(2021, 4, 8, 10),
-    endDate: new Date(2021, 4, 8, 11),
-  },
-  {
-    title: "Making",
-    startDate: new Date(2021, 4, 8, 11),
-    endDate: new Date(2021, 4, 8, 24),
-  },
-  {
-    title: "Making",
-    startDate: new Date(2021, 4, 9, 0),
-    endDate: new Date(2021, 4, 9, 11),
-  },
-  {
-    title: "Lunch",
-    startDate: new Date(2021, 4, 8, 12),
-    endDate: new Date(2021, 4, 8, 13),
-  },
-  {
-    title: "Dinner",
-    startDate: new Date(2021, 4, 8, 18),
-    endDate: new Date(2021, 4, 8, 19),
-  },
-  {
-    title: "Night Treat",
-    startDate: new Date(2021, 4, 8, 21),
-    endDate: new Date(2021, 4, 8, 22),
-  },
-  {
-    title: "Breakfast",
-    startDate: new Date(2021, 4, 9, 7),
-    endDate: new Date(2021, 4, 9, 8),
-  },
-  {
-    title: "Lunch",
-    startDate: new Date(2021, 4, 9, 11, 30),
-    endDate: new Date(2021, 4, 9, 12),
-  },
-  {
-    title: "Expo",
-    startDate: new Date(2021, 4, 9, 12),
-    endDate: new Date(2021, 4, 9, 13),
-  },
-  {
-    title: "Top 8 Demo",
-    startDate: new Date(2021, 4, 9, 16),
-    endDate: new Date(2021, 4, 9, 17),
-  },
-  {
-    title: "Award Ceremony",
-    startDate: new Date(2021, 4, 9, 17),
-    endDate: new Date(2021, 4, 9, 18),
-  },
-  {
-    title: "Ending",
-    startDate: new Date(2021, 4, 9, 18),
-    endDate: new Date(2021, 4, 9, 19),
-  },
-];
+const Appointment = ({ children, style, ...restProps }) => (
+  <Appointments.Appointment
+    {...restProps}
+    style={{
+      ...style,
+      backgroundColor: "#0081A8", // blue
+      // backgroundColor: "#CC0049", // red
+    }}
+  >
+    {children}
+  </Appointments.Appointment>
+);
 
 export default function Home() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Paper>
-        <Scheduler data={appointments}>
-          <ViewState currentDate="2021-05-08" />
-          <DayView startDayHour={8} endDayHour={24} />
-          <Appointments />
-        </Scheduler>
-        <Scheduler data={appointments}>
-          <ViewState currentDate="2021-05-09" />
-          <DayView startDayHour={0} endDayHour={24} />
-          <Appointments />
-        </Scheduler>
-      </Paper>
-    </MuiThemeProvider>
+    <Paper>
+      <Scheduler data={data}>
+        <ViewState currentDate="2021-05-08" />
+        <DayView intervalCount={2} startDayHour={8.5} endDayHour={21.5} />
+        <Appointments appointmentComponent={Appointment} />
+      </Scheduler>
+    </Paper>
   );
 }
