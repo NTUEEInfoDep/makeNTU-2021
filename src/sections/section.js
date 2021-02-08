05cf1f098552b6fbcfa9025cc5763312982204f1
Hsuan-Yu Chou <syuanyujhou@gmail.com>
2021-02-09 05:07:11 +0800

    mod section.js

--- src/sections/section.js
+++ src/sections/section.js
@@ -1,39 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Hero from "sections/hero";
 import About from "sections/about";
-import Testimonials from "sections/testimonials";
-import Services from "sections/services";
-import Pricing from "sections/pricing";
 import Contact from "sections/contact";
+import Hero from "sections/hero";
+import MatUITimeline from "sections/matuitimeline";
 import PageNotFound from "sections/pageNotFound";
+import Pricing from "sections/pricing";
+import Services from "sections/services";
+import Testimonials from "sections/testimonials";
 
 // Dynamically import or require sections inside the section folder
 const components = {
-    Hero,
-    About,
-    Testimonials,
-    Services,
-    Pricing,
-    Contact,
-    PageNotFound,
+  Hero,
+  About,
+  Testimonials,
+  Services,
+  Pricing,
+  Contact,
+  PageNotFound,
+  MatUITimeline,
 };
 
 const Section = ({ contentModuleId, type }) => {
-    const component = type.split("Layout")[1];
+  const component = type.split("Layout")[1];
 
-    if (typeof components[component] === "undefined") {
-        return "";
-    }
+  if (typeof components[component] === "undefined") {
+    return "";
+  }
 
-    return React.createElement(components[component], {
-        contentModuleId,
-    });
+  return React.createElement(components[component], {
+    contentModuleId,
+  });
 };
 
 Section.prototype = {
-    contentModuleId: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
+  contentModuleId: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default Section;
